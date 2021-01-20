05edd19b2f1bccaff1f66a8a8a761b370727fb14
Fatih <kresnofatihimani.private@gmail.com>
2021-01-20 08:53:11 +0700

    finished learning useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 
+function reducer(state, action){
+  switch (action.type) {
+    case "minus":
+      return {number: state.number-1}
+    case "plus":
+      return {number: state.number+1}
+  }
+}
+
 function App() {
+  // useReducer Method
+  const [state, dispatch] = useReducer(reducer, {number:0})
+  function minus2(){
+    dispatch({type: "minus"})
+  }
+  function plus2(){
+    dispatch({type: "plus"})
+  }
+  // useState Method
+  const [number, setNumber] = useState(0);
+  function minus(){
+    setNumber(number-1)
+  }
+  function plus(){
+    setNumber(number+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Haha
-        </a>
-      </header>
+      {/* useState Method */}
+      <button onClick={minus}>-</button>
+        <p>{number}</p>
+      <button onClick={plus}>+</button>
+      {/* useReducer Method */}
+      <button onClick={minus2}>-</button>
+        <p>{state.number}</p>
+      <button onClick={plus2}>+</button>
     </div>
   );
 }
