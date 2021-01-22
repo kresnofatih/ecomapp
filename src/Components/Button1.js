b4bd96e05e0a03e8d70138ad9ead3f0e977ebb16
Fatih <kresnofatihimani.private@gmail.com>
2021-01-22 14:43:10 +0700

    created button1

---
+++ src/Components/Button1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button1.css';
+
+function Button1({name}) {
+    return (
+        // <a href="" className="button btn1">{name}</a>
+        // <a href="" className="button btn2">{name}</a>
+        // <a href="" className="button btn3">{name}</a>
+        <a href="" className="button btn4">{name}</a>
+    )
+}
+
+export default Button1
